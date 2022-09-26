35687b30437aff0ab577945c2a0bea698e6d4402
Chamath Viduranga <chamath.viduranga@airudi.com>
2022-09-27 00:02:47 +0530

    adding angular UI changes

--- portals/pos-angular/src/app/app.module.ts
+++ portals/pos-angular/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ItemModule } from './item/item.module';
+import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    FontAwesomeModule,
+    ItemModule,
+    AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
