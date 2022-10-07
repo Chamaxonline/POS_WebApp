9d5c02f92bb096fe526b8674ef83827a7e08cc6a
Chamath Viduranga <chamath.viduranga@airudi.com>
2022-10-08 00:20:48 +0530

    item

--- portals/pos-angular/src/app/item/components/item-management/item-create/item-create.component.ts
+++ portals/pos-angular/src/app/item/components/item-management/item-create/item-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-item-create',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemCreateComponent implements OnInit {
 
+  
+  customerFrom: FormGroup | undefined;
   constructor() { }
 
   ngOnInit(): void {
-  }
+    
+  //   this.customerFrom = this.formBuilder.group({
+  //     customerId: [0],
+  //     fname: ['', Validators.required],
+  //     lname: ['', Validators.required],
+  //     email: ['', [Validators.required, Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+  //     addressLine1: ['', Validators.required],
+  //     addressLine2: ['LK', Validators.required],
+  //     addressLine3: ['IO'],
+  //     mobileNumber: ['', Validators.required],
+  //     password: ['', Validators.required],
+  //     confirmPassword: ['', Validators.required],
+  //     active: [true, Validators.required],
+  //     ipAddress: ['192'],
+  //     customerGroupId: [0],
+   }
+  closeModal(){
 
+  }
 }
